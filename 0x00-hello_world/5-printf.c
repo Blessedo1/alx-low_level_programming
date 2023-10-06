00f326af36c2bc5dcd2d359fb3b821ef15b6f9f6
root <adesinablessed19@gmail.com>
2023-10-05 19:18:35 -0700

    updated 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main(void)/*this function prints a string using the printf*/
+int main(void)/* this function prints a string using the printf */
 {
-	printf("with proper grammar, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
