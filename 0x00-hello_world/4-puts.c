09d566d61b249870fd17bbb30f49dbbe727e7d93
root <adesinablessed19@gmail.com>
2023-10-05 20:34:25 -0700

    4-puts.c created

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * puts - this prints a string and adds a new line until program ends with 0
+ * main - this funtion is the entr point for our function
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
