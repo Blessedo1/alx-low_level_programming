02a742b85077acf7ee7ae6da683f782b1187640a
root <adesinablessed19@gmail.com>
2023-10-05 19:55:17 -0700

    updated 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main(void)/* this function prints a string using the printf */
+/**
+ * main function is the entry point of this program
+ * print function prints the formatted text to standard output
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
