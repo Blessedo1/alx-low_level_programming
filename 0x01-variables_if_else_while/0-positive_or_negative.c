f5e2d72eec96fc2a921c75dbd84e1e2968c70385
root <adesinablessed19@gmail.com>
2023-10-06 19:08:32 -0700

    0-positive_or_negative.c and README.md files created

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* This function assigns number to the variable n and ates whether the number is positive or negative each time it is executed */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	printf("%d is positive\n", n)
+	else if (n == 0)
+	printf("%d is zero\n", n)
+	else
+	printf("%d is negative\n", n)
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+0x01-variables_if_else_while contains tasks on conditional statements in low level programming
