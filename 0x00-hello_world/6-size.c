067c79d20539fc37e002a5de4017e3ec008ea08d
root <adesinablessed19@gmail.com>
2023-10-09 09:19:01 -0700

    changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,20 +3,20 @@
 /**
  * main - Entry point
  *
- * printf - prints the string
+ * printf - prints the size in bytes
  *
- * return - always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of a short: %lu byte(s)\n", sizeof(short));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-	printf("Size of a double: %lu byte(s)\n", sizeof(double));
-	printf("Size of a long double: %lu byte(s)\n", sizeof(long double));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of a short: %zu byte(s)\n", sizeof(short));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a double: %zu byte(s)\n", sizeof(double));
+	printf("Size of a long double: %zu byte(s)\n", sizeof(long double));
 
 	return (0);
 }
