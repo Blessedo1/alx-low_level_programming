a0d89b86c55c7b278f4c7f49602ec7f2069c9b99
root <adesinablessed19@gmail.com>
2023-10-09 17:32:01 -0700

    4-print_alphabt.c file created and compiled

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * putchar - prints the alphabet in lowercase except q and e
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+        char alphabet[] = "abcdfghijklmnoprstuvwxyz";
+        int i = 0;
+
+        while (alphabet[i] != '\0')
+        {
+                putchar(alphabet[i]);
+                i++;
+        }
+
+        putchar('\n');
+
+        return (0);
+}
