c96e81603ba3f88faf4049afe17bb79e832f8f90
root <adesinablessed19@gmail.com>
2023-10-09 18:54:12 -0700

    6-print_numberz.c file created and compiled

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * printf - prints all single digit base 10 numbers starting from 0
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+        int i;
+
+        for (i = 0; i < 10; i++)
+        {
+                putchar(i);
+        }
+        putchar('\n');
+
+        return (0);
+}
