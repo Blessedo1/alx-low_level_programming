3fcc196c3feef9a81ac1d21e9d6cc2feb7425ef9
root <adesinablessed19@gmail.com>
2023-10-11 19:51:25 -0700

    changes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,17 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
-	char j;
-	i = 0;
-	while (i < 10)
-	{
-		for (j = 'a'; j <= 'z'; j++)
-		{
-			_putchar(j);
-		}
-		_putchar('\n');
-		i++;
-	}
+int i;
+char j;
+i = 0;
+while (i < 10)
+{
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+i++;
+}
 
 }
