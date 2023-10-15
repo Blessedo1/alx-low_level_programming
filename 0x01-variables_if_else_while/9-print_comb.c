68d24ec05fc6a55e38527ef85e79d50ebea96fc5
root <adesinablessed19@gmail.com>
2023-10-15 16:30:54 -0700

    9-print_comb.c file created and compiled

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * putchar - prints all possible combinations of single-digit numbers
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+
+		if (i < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
+		
