ca57078610138a780e4b7bffae507ddf11f5144d
root <adesinablessed19@gmail.com>
2023-10-16 21:25:33 -0700

    5-rev_string.c file created and compiled

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,9 +2,23 @@
 
 /**
  * rev_string - reverses a string
- *
+ * @s - string to be reversed
  * Return: null
  */
 void rev_string(char *s)
 {
+	int length = 0;
+	int i;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = 0; i < length / 2; i++)
+	{
+		char temp = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i -1] = temp;
+	}
 }
