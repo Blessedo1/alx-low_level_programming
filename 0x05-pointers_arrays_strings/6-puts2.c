d77224363605fa3e531310d789e0600d3e95e27a
root <adesinablessed19@gmail.com>
2023-10-16 21:45:32 -0700

    6-puts2.c file created and compiled

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,9 +2,22 @@
 
 /**
  * puts2 - prints every character of a string
- *
+ * @str: string to be printed
  * Return: null
  */
 void puts2(char *str)
 {
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
