e3d6bfb4c3a677cb2d299b8b9d84429b020493e6
root <adesinablessed19@gmail.com>
2023-10-19 19:43:09 -0700

    2-strncpy.c file created and compiled

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,11 +2,25 @@
 
 /**
  * *_strncpy - copies a string
- * @*dest: string to be copied to
- * @*src: string to be copied
+ * @dest: string to be copied to
+ * @src: string to be copied
  * @n: maximum bytes of src
- * Return: Always 0.
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+	return (dest);
 }
