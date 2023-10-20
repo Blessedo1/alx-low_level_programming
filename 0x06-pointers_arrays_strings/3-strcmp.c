f2006c85a213df7571ac781746f0eb7433b705ad
root <adesinablessed19@gmail.com>
2023-10-19 19:52:13 -0700

    3-strcmp.c file created and compiled

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,10 +2,22 @@
 
 /**
  * _strcmp - compares two strings
- * @*s1: first string to be compared
- * @*s2: second string to be compared
+ * @s1: first string to be compared
+ * @s2: second string to be compared
  * Return:
  */
 int _strcmp(char *s1, char *s2)
 {
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
 }
