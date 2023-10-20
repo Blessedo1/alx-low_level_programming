9d21c7b2b2a2b244cc51a0d5d81c23c49b6bf7aa
root <adesinablessed19@gmail.com>
2023-10-19 19:34:59 -0700

    1-strncat.c file created and compiled

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,11 +2,28 @@
 
 /**
  * *_strncat - concatenates two strings
- * @*dest: string to be appended to
- * @*src: string to be appended
+ * @dest: string to be appended to
+ * @src: string to be appended
  * @n: maximum bytes to be used
- * Return:
+ * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
