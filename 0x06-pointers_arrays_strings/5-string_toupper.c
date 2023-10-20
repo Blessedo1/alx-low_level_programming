59812fe376999404e8c18c4c12de483ec68ee497
root <adesinablessed19@gmail.com>
2023-10-19 20:15:52 -0700

    5-string_toupper.c file created and compiled

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,6 +7,16 @@
  *
  * Return: Always 0.
  */
-char *string_toupper(char *)
+char *string_toupper(char *n)
 {
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
 }
