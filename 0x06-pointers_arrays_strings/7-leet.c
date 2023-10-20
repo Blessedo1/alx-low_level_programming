9809101e9dcd6ea4015129b7c31e479140b5e5e4
root <adesinablessed19@gmail.com>
2023-10-19 20:59:48 -0700

    7-leet.c file created and compiled

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,8 +4,23 @@
  * *leet - encode a string into 1337
  * @*: string to be encoded
  *
- * Return: Always 0.
+ * Return: return n
  */
-char *leet(char *)
+char *leet(char *n)
 {
+	int i;
+	int j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
+	}
+	return (n);
 }
