2c20d7203562ed2e7d3f97689eec24368a664a37
root <adesinablessed19@gmail.com>
2023-10-25 17:32:28 -0700

    changes

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - A function that checks for lowercase letter
- * c: the lowercase character to be checked
+ * @c: the lowercase character to be checked
  * Return: returns 1 if c is lowercase, and 0 if otherwise
  */
 int _islower(int c)
