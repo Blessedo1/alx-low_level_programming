c9b54b8d2f42241c46706b21561da2cacffe4df7
root <adesinablessed19@gmail.com>
2023-11-08 15:08:49 -0800

    2-main.c file created and compiled

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
