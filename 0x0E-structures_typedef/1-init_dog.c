89116f8d6b8b793d9a0f89b6fdd00f46b7601e58
root <adesinablessed19@gmail.com>
2023-11-21 14:32:33 -0800

    1-init_dog.c file created and compiled

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of struct dog
+ * @d: pointer to be initialized
+ * @name: variable to be initialized to
+ * @age: variable to be initialized to
+ * @owner: variable to be initialized to
+ * Return: Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
+
