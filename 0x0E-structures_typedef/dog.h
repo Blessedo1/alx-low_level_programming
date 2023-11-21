ce90bca52576927f6bc1207c6f4a31a5207e021f
root <adesinablessed19@gmail.com>
2023-11-21 12:36:42 -0800

    dog.h header file and README.md file created

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_HEADER
+#define DOG_HEADER
+
+/**
+ * struct dog - This struct contains information about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
