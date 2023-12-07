8786dcd6ebfeb391e5a26fcc973c952965d6d4dd
root <adesinablessed19@gmail.com>
2023-12-07 11:39:06 -0800

    changes

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * main - prints all arguments it receives including the first one
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: return 0
+ */
+int main(int argc, char **argv)
+{
+	int i = 0;
+	while(i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
