733369881cbe83758dd19e3d82fbaa272dd0b3ed
Blessed Adesina <adesinablessed19@gmail.com>
2024-01-31 10:08:32 -0500

    8-print_square.c corrected

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,10 +10,14 @@ void print_square(int size)
 	int i;
 	int j;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-			_putchar('#');
-		_putchar('\n');
-	}
+	if (size > 0)
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			if (i == size - 1)
+				continue;
+			_putchar('\n');
+		}
+	_putchar('\n');
 }
