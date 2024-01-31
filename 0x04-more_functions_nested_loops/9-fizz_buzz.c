dd7d878b9d76c4f1830ca96cc64187586f40fd22
Blessed Adesina <adesinablessed19@gmail.com>
2024-01-31 10:32:58 -0500

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1 to 100 with multiples of 3: Fizz, 5: Buzz, both: FizzBuzz
+ * Return - Always return 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0 && i % 5 == 0)
+                        printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
