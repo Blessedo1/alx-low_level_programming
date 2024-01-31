0032c3204392956a24a704ef520e44fe894c29f7
Blessed Adesina <adesinablessed19@gmail.com>
2024-01-30 21:47:38 -0500

    1-isdigit.c corrected

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,12 +2,12 @@
 
 /**
  * _isdigit - check for a digit
- *
+ * @c: digit to be checked
  * Return: 1 if a digit and 0 if otherwise
  */
 int _isdigit(int c)
 {
-	if(c >= 48 && c <= 57)
+	if (c >= 48 && c <= 57)
 		return (1);
 	return (0);
 }
