561d168a3d76cb0af1a582518d338fc76bed76b3
Blessed Adesina <adesinablessed19@gmail.com>
2024-12-02 23:49:33 +0100

    code changes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: first string.
+ * @accept: second string.
+ * Return: a pointer to the byte in s that matches one of the
+ * bytes in accept, or NULL if no such byte is found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *reset_accept;
+
+	while (*s)
+	{
+		reset_accept = accept;
+		while (*reset_accept)
+		{
+			if (*s == *reset_accept)
+				return (s);
+			reset_accept++;
+		}
+		s++;
+	}
+	
+	return ('\0');
+}
